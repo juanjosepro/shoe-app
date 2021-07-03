7dce0f5c616920dcf7aa4abfa84fd086cf9f99ff
Juan Jose Pau <juanjosedev@gmail.com>
2021-07-03 17:38:21 -0500

    rename domain

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 # load production server from .env
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
-ALLOWED_HOSTS = ['shoe-apps.herokuapp.com']
+ALLOWED_HOSTS = ['shoe-app.juanjosepau.com']
 
 # Message
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
