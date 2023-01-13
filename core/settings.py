6cc6416d741528d3d32805bac977f432b7473b36
Juan José Pau <53390329+JuanJoseStone@users.noreply.github.com>
2023-01-12 21:08:32 -0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'apps',
+    # 'apps',
     'apps.sizes',
     'apps.category',
     'apps.model',
